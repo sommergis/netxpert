c6941ecdd60146e56fac3fcc23c5da2127bece00
johannes <22645408+sommergis@users.noreply.github.com>
2015-07-06 22:01:17 +0200

    core solver integriert

---
+++ include/solver/isptree.h
@@ -0,0 +1,30 @@
+#ifndef ISPTREE_H
+#define ISPTREE_H
+
+namespace netxpert {
+    /** Abstract (Interface) Class for all Shortest Path Tree Solvers
+    */
+    class ISPTree
+    {
+        public:
+
+            /** Default destructor */
+            virtual ~ISPTree() {}
+            /* start of MCFClass SPTree Interface */
+            virtual void LoadNet(unsigned int nmx, unsigned int mmx, unsigned int pn, unsigned int pm, double* pU,
+                            double* pC, double* pDfct, unsigned int* pSn, unsigned int* pEn)=0;
+            virtual unsigned int MCFmmax()=0;
+            virtual unsigned int MCFnmax()=0;
+            virtual void ShortestPathTree()=0;
+            virtual void SetOrigin( unsigned int NewOrg )=0;
+            virtual void SetDest( unsigned int NewDst )=0;
+            virtual bool Reached( unsigned int NodeID )=0;
+            virtual unsigned int* ArcPredecessors( void )=0;
+            virtual unsigned int* Predecessors( void )=0;
+            virtual void GetArcPredecessors ( unsigned int *outArcPrd )=0;
+            virtual void GetPredecessors( unsigned int *outPrd )=0;
+            //virtual void GetPath ( unsigned int Dst, unsigned int *outSn, unsigned int *outEn );
+            /* end of MCFClass SPTree Interface */
+    };
+}
+#endif // ISPTREE_H
