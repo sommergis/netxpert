4674228eede1aa9780a5176e44b2645658dea291
johannes <22645408+sommergis@users.noreply.github.com>
2015-07-22 22:21:59 +0200

    Transportation Solver init

---
+++ include/solver/imcflow.h
@@ -0,0 +1,28 @@
+#ifndef IMINCOSTFLOW_H
+#define IMINCOSTFLOW_H
+
+namespace netxpert {
+    /**
+    * \Class Abstract Class (Interface) for all Minimum Cost Flow Problem Solvers
+    */
+    class IMinCostFlow
+    {
+        public:
+            virtual ~IMinCostFlow(){}
+            /* start of MCFClass MCFsimplex Interface */
+            virtual void MCFArcs(unsigned int* outStartNodes, unsigned int* outEndNodes)=0;
+            virtual void LoadNet(unsigned int nmx, unsigned int mmx, unsigned int pn, unsigned int pm, double* pU,
+                            double* pC, double* pDfct, unsigned int* pSn, unsigned int* pEn)=0;
+            virtual unsigned int MCFmmax()=0;
+            virtual unsigned int MCFnmax()=0;
+            virtual void SolveMCF()=0;
+            virtual double MCFGetFO()=0;
+            virtual int MCFGetStatus()=0;
+            virtual void MCFCosts(double* outCosts)=0;
+            virtual void MCFGetX(double* outFlow)=0;
+            virtual void CheckPSol()=0;
+            virtual void CheckDSol()=0;
+            /* end of MCFClass MCFsimplex Interface */
+    };
+}
+#endif // IMINCOSTFLOW_H
