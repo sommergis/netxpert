dd56fa4079fb0ce29be6ce2d74bd7c2e5639e861
johannes <22645408+sommergis@users.noreply.github.com>
2015-10-03 16:05:06 +0200

    Initial add of MCFP Simple

---
+++ src/simple/mcfp_simple.cpp
@@ -0,0 +1,196 @@
+#include "mcfp_simple.h"
+
+netxpert::simple::MinCostFlow::MinCostFlow(std::string jsonCnfg)
+{
+
+}
+
+int netxpert::simple::MinCostFlow::Solve()
+{
+    using namespace netxpert; //local scope!
+
+    try
+    {
+        Config cnfg = NETXPERT_CNFG;
+
+        //1. Config
+        if (!DBHELPER::IsInitialized)
+        {
+            DBHELPER::Initialize(cnfg);
+        }
+
+        try
+        {
+            if (!LOGGER::IsInitialized)
+            {
+                LOGGER::Initialize(cnfg);
+            }
+        }
+        catch (exception& ex)
+        {
+            cout << "Error creating log file: " + cnfg.LogFileFullPath << endl;
+            cout << ex.what() << endl;
+        }
+
+        InputArcs arcsTable;
+        string arcsGeomColumnName = cnfg.ArcsGeomColumnName;
+
+        string pathToSpatiaLiteDB = cnfg.NetXDBPath;
+        string arcsTableName = cnfg.ArcsTableName;
+
+        string nodesTableName = cnfg.NodesTableName;
+        string nodesGeomColName = cnfg.NodesGeomColumnName;
+        string resultTableName = cnfg.ResultTableName.empty() ? cnfg.ArcsTableName + "_mcf" : cnfg.ResultTableName;
+        //bool dropFirst = true;
+
+        bool autoCleanNetwork = cnfg.CleanNetwork;
+
+        ColumnMap cmap { cnfg.ArcIDColumnName, cnfg.FromNodeColumnName, cnfg.ToNodeColumnName,
+                        cnfg.CostColumnName, cnfg.CapColumnName, cnfg.OnewayColumnName,
+                        cnfg.NodeIDColumnName, cnfg.NodeSupplyColumnName };
+
+        bool withCapacity = false;
+        if (!cnfg.CapColumnName.empty())
+            withCapacity = true;
+
+        //2. Load Network
+        DBHELPER::OpenNewTransaction();
+        LOGGER::LogInfo("Loading Data from DB..!");
+        arcsTable = DBHELPER::LoadNetworkFromDB(arcsTableName, cmap);
+        LOGGER::LogInfo("Done!");
+        Network net (arcsTable, cmap, cnfg);
+
+        LOGGER::LogInfo("Converting Data into internal network..");
+        net.ConvertInputNetwork(autoCleanNetwork);
+
+        DBHELPER::CommitCurrentTransaction();
+        DBHELPER::CloseConnection();
+        LOGGER::LogInfo("Done!");
+
+        //MST Solver
+        solver = unique_ptr<netxpert::MinCostFlow>(new netxpert::MinCostFlow (cnfg));
+        auto& mcf = *solver;
+        mcf.Solve(net);
+        LOGGER::LogInfo("Done!");
+
+        vector<FlowCost> mcfResult = mcf.GetMinCostFlow();
+
+        LOGGER::LogInfo("Optimum: " + to_string(mcf.GetOptimum()));
+        LOGGER::LogInfo("Count of MCF: " + to_string(mcfResult.size()) );
+
+        unique_ptr<DBWriter> writer;
+        unique_ptr<SQLite::Statement> qry;
+        switch (cnfg.ResultDBType)
+        {
+            case RESULT_DB_TYPE::SpatiaLiteDB:
+            {
+                if (NETXPERT_CNFG.ResultDBPath == NETXPERT_CNFG.NetXDBPath)
+                {
+                    //Override result DB Path with original netXpert DB path
+                    writer = unique_ptr<DBWriter>(new SpatiaLiteWriter(cnfg, NETXPERT_CNFG.NetXDBPath));
+                }
+                else
+				{
+                    writer = unique_ptr<DBWriter>(new SpatiaLiteWriter(cnfg));
+				}
+                writer->CreateNetXpertDB(); //create before preparing query
+                writer->OpenNewTransaction();
+                writer->CreateSolverResultTable(resultTableName, true);
+                writer->CommitCurrentTransaction();
+                /*if (cnfg.GeometryHandling != GEOMETRY_HANDLING::RealGeometry)
+                {*/
+                auto& sldbWriter = dynamic_cast<SpatiaLiteWriter&>(*writer);
+                qry = unique_ptr<SQLite::Statement> (sldbWriter.PrepareSaveResultArc(resultTableName));
+                //}
+            }
+                break;
+            case RESULT_DB_TYPE::ESRI_FileGDB:
+            {
+                writer = unique_ptr<DBWriter> (new FGDBWriter(cnfg)) ;
+                writer->CreateNetXpertDB();
+                writer->OpenNewTransaction();
+                writer->CreateSolverResultTable(resultTableName, true);
+                writer->CommitCurrentTransaction();
+            }
+                break;
+        }
+
+        LOGGER::LogDebug("Writing Geometries..");
+        //writer->OpenNewTransaction();
+        //Processing and Saving Results are handled within net.ProcessResultArcs()
+
+        string arcIDs;
+        int counter = 0;
+        for (FlowCost& arcFlow : mcfResult)
+        {
+            counter += 1;
+            if (counter % 2500 == 0)
+                LOGGER::LogInfo("Processed #" + to_string(counter) + " geometries.");
+
+            string arcIDs = "";
+            InternalArc key = arcFlow.intArc;
+            double cost = arcFlow.cost;
+            double flow = arcFlow.flow;
+            double cap = -1;
+            vector<InternalArc> arc { key };
+
+            vector<ArcData> arcData = net.GetOriginalArcData(arc, cnfg.IsDirected);
+            // is only one arc
+            if (arcData.size() > 0)
+            {
+                ArcData arcD = *arcData.begin();
+                arcIDs = arcD.extArcID;
+                cap = arcD.capacity;
+            }
+
+            string orig;
+            string dest;
+            try{
+                orig = net.GetOriginalStartOrEndNodeID(key.fromNode);
+            }
+            catch (exception& ex) {
+                orig = net.GetOriginalNodeID(key.fromNode);
+            }
+            try{
+                dest = net.GetOriginalStartOrEndNodeID(key.toNode);
+            }
+            catch (exception& ex) {
+                dest = net.GetOriginalNodeID(key.fromNode);
+            }
+            net.ProcessResultArcs(orig, dest, cost, cap, flow, arcIDs, arc, resultTableName, *writer);
+        }
+        writer->CommitCurrentTransaction();
+        writer->CloseConnection();
+        LOGGER::LogDebug("Done!");
+
+        return 0; //OK
+    }
+    catch (exception& ex)
+    {
+        LOGGER::LogError("MinCostFlow_Simple::Solve() - Unexpected Error!");
+        LOGGER::LogError(ex.what());
+        return 1; //Not OK
+    }
+}
+
+double netxpert::simple::MinCostFlow::GetOptimum()
+{
+    double result = 0;
+    if (this->solver)
+        result = this->solver->GetOptimum();
+    return result;
+}
+
+std::string netxpert::simple::MinCostFlow::GetMinimumCostFlowAsJSON()
+{
+    std::string result;
+
+    return result;
+}
+
+std::vector<netxpert::FlowCost> netxpert::simple::MinCostFlow::GetMinimumCostFlow()
+{
+    std::vector<netxpert::FlowCost>  result;
+
+    return result;
+}
